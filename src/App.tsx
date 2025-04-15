c4d5a48b3f859408247a0838759ec5a85ea9bb00
MariaHajdic <marianneliash@gmail.com>
2025-04-15 22:16:33 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, World!</h1>
+        <p>
+          This is a basic React application.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
